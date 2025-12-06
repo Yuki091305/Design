a796b2f7ac8eea02e335317e9b8eaa108ed4da2c
Yuki091305 <yukifieri@gmail.com>
2025-12-06 11:49:10 +0700

    юрл каталоги обновленные

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('user/dashboard/', views.user_dashboard, name='user_dashboard'),
     path('request/create/', views.request_create, name='request_create'),
     path('request/<uuid:pk>/delete/', views.request_delete, name='request_delete'),
+    path('request/<uuid:pk>/', views.request_detail, name='request_detail'),
     path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('admin/request/<uuid:pk>/update/', views.admin_update_status, name='admin_update_status'),
 ]
